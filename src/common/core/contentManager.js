3cdf3ba762e43fa894e7ce9c4384030b0a6da465
vntzy <vntzyv@gmail.com>
2015-04-26 02:16:51 +0300

    add contentManager class

---
+++ src/common/core/contentManager.js
@@ -0,0 +1,13 @@
+/**
+/* Responsible for managing the toxic or potencially toxic content's state
+**/
+function ContentManager(domElement) {
+
+  this.hide = function() {
+
+  }
+
+  this.show = function() {
+
+  }
+}
--- src/common/core/contentMarker.js
+++ src/common/core/contentMarker.js
@@ -7,15 +7,15 @@ function ContentMarker(markAsAcceptableCallback, markAsOffensiveCallback) {
 
   this.mark = function(domElement, isBlocked) {
     var dom = $(domElement);
-        var children = dom.children();
-        children.wrapAll('<div class="t-blocked"></div>');
-        var hiddenDom = children().first();
-        hiddenDom.hide();
-        dom.prepend('<div style="text-align: center;>
-                      <img style="float: left; width: 20px; height: 20px;" src="http://vignette3.wikia.nocookie.net/yogbox/images/c/c6/Ban.png/revision/latest?cb=20120816112127">
-                      Blocked Content
-                      <button style="float: right;">Unblock</button>
-                      <button style="float: right;">Show</button>
-                    </div>');
+    var children = dom.children();
+    children.wrapAll('<div class="t-blocked"></div>');
+    var hiddenDom = children().first();
+    hiddenDom.hide();
+    dom.prepend('<div style="text-align: center;>
+                  <img style="float: left; width: 20px; height: 20px;" src="http://vignette3.wikia.nocookie.net/yogbox/images/c/c6/Ban.png/revision/latest?cb=20120816112127">
+                  Blocked Content
+                  <button style="float: right;">Unblock</button>
+                  <button style="float: right;">Show</button>
+                </div>');
   }
 }
