2ea46e90b77b1459bbdd35ca838c16dae6492c91
George Koykov <genetix.ssh@gmail.com>
2015-04-25 21:39:18 +0300

    Added options page and option storage functionality. The only working option is aggressiveness

--- src/common/main.js
+++ src/common/main.js
@@ -1,13 +1,28 @@
 ﻿function Extension() {
     var self = this;
 	
+	self._loadOptions();
+	
     kango.ui.browserButton.addEventListener(kango.ui.browserButton.event.COMMAND, function() {
         self._onCommand();
     });
 }
 
 Extension.prototype = {
-
+	
+	_defaultOptions: {
+		"aggressiveness" = 50,
+	},
+	
+	_loadOptions: function() {
+		
+		// aggressiveness threshold
+		aggressiveness = kango.storage.getItem("aggressiveness");
+		if(aggressiveness == null) {
+			kango.storage.setItem("aggressiveness", this._defaultOptions["aggressiveness"]);
+		}
+	},
+	
     _onCommand: function() {
         kango.browser.tabs.create({url: 'https://github.com/vntzy/Anti-Troll'});
     }
--- src/common/ui/options.js
+++ src/common/ui/options.js
@@ -1,56 +1,66 @@
 ﻿var OptionsStorage = {
-
-    init: function() {
-        $('#storage-get').click(function(event) {
-            OptionsStorage.testGet();
-        });
-
-        $('#storage-set').click(function(event) {
-            OptionsStorage.testSet();
-        });
-
-        $('#storage-remove').click(function(event) {
-            OptionsStorage.testRemove();
-        });
-
-        $('#storage-keys').click(function(event) {
-            OptionsStorage.testKeys();
-        });
-    },
-
-    testGet: function() {
-        kango.invokeAsync('kango.storage.getItem', $('#storage-key').val(), function(value) {
-            $('#storage-value').val(value || 'null');
-        });
-    },
-
-    testSet: function() {
-        kango.invokeAsync('kango.storage.setItem', $('#storage-key').val(), $('#storage-value').val());
-    },
-
-    testRemove: function() {
-        kango.invokeAsync('kango.storage.removeItem', $('#storage-key').val(), function() {
-            $('#storage-value').val('null');
-        });
-    },
-
-    testKeys: function() {
-        kango.invokeAsync('kango.storage.getKeys', function(keys) {
-            $('#storage-value').val(keys.toString());
-        });
-    }
+	
+	_storageKeyPrefix: "option_",
+	
+	_applyPrefix: function(key) {
+		return this._storageKeyPrefix.concat(key);
+	},
+	
+	init: function() {
+		// TODO
+	},
+	
+	updateSetting: function(key, value) {
+		kango.invokeAsync('kango.storage.setItem', this._applyPrefix(key), value);
+	},
+	
+	getSetting: function(key) {
+		return kango.storage.getItem(this._applyPrefix(key));
+	}
 };
 
-KangoAPI.onReady(function() {
-	// Assures that jquery is loaded into options page
-	$( "#slider" ).slider();
+var AggressivenessSlider = {
+	_value: 50, // Default value
+	_minValue: 1,
+	_maxValue: 100,
+	_labelScale: [
+		"low",
+		"moderate",
+		"high"
+	],
 	
-    $('#ready').show();
-    $('#not-ready').hide();
+	_updateLabel: function() {
+		var scaleStep = Math.ceil(this._maxValue / this._labelScale.length);
+		$("#aggressiveness").val(this._labelScale[Math.floor(this._value/scaleStep)]);
+	},
+	
+	init: function() {
+		// Get value from user saved data
+		this._value = OptionsStorage.getSetting("aggressiveness");
+		
+		$("#slider").slider({
+			range: "min",
+			value: this._value,
+			min: this._minValue,
+			max: this._maxValue,
+			slide: function(event, ui) {
+				AggressivenessSlider._value = ui.value;
+				AggressivenessSlider._updateLabel();
+				OptionsStorage.updateSetting("aggressiveness", AggressivenessSlider._value);
+			}
+		});
+		
+		this._updateLabel();
+	}
+}
 
+KangoAPI.onReady(function() {
     $('#close').click(function(event) {
         KangoAPI.closeWindow()
     });
 
     OptionsStorage.init();
+	
+	// Initializing slider for user aggressiveness threshold
+	AggressivenessSlider.init();
 });
