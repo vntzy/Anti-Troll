dad6907359016d3002142d0b7aed5032fb445ee3
Yavor <yavorpap@abv.bg>
2015-04-26 00:44:22 +0300

    Semi-connected version of core logic (except for the classifier)

--- src/common/core/content_scripts/dom_event_handler.js
+++ src/common/core/content_scripts/dom_event_handler.js
@@ -4,33 +4,71 @@
 // @include https://*
 // @include about:blank
 // @require dependencies/jquery-2.1.3.min.js
+// @require core/utils.js
 // ==/UserScript==
 
 var $ = window.$.noConflict(true); // Required for IE
 
-var _dom_event_handler = function(mutations) {
+var _reportFalsePositive = function(domElement) {
+    //TODO: Work
+};
+
+var _reportFalseNegative = function(domElement) {
+    //TODO: Work
+};
+
+var _domEventHandler = function(mutations) {
+    var targetDomElements = [];
     for(var i = 0; i < mutations.length; i++) {
         var mutationRecord = mutations[i];
         switch(mutationRecord.type) {
+            case "bodyInit":
             case "characterData":
-                kango.console.log("Character Data");
-                kango.console.log(mutationRecord.target);
-                break;
             case "childList":
-                kango.console.log("Child list");
-                kango.console.log(mutationRecord.target);
+                //kango.console.log("Character Data");
+                //kango.console.log(mutationRecord.target);
+                _append(targetDomElements, mutationRecord.target);
                 break;
             default:
                 break;
         }
     }
+    kango.console.log(targetDomElements);
+    var contentElements =
+        contentFilter.findAllContentOnDOMElements(targetDomElements, document);
+    //TODO: Work for real
+    kango.console.log(contentElements);
+    return;
+    for (var i = 0; i < contentElements.length; i++) {
+        var domElement = contentElements[i];
+        var plaintextValue = _renderTextFromDomElement(domElement);
+        kango.invokeAsync("globalClassifier.isContentAcceptable",
+            plaintextValue, function(isToxic) {
+                kango.console.log(plaintextValue + "|" + isToxic);
+                contentMarker.mark(domElement, isToxic);
+            });
+    }
 };
 
-var observer = new MutationObserver(_dom_event_handler);
+var observer = new MutationObserver(_domEventHandler);
 
 // MutationObserverInit configuration:
 var observerInit = {
     attributes: false, childList: true, characterData: true, subtree: true
 };
 
+var contentMarker =
+    new ContentMarker(_reportFalsePositive, _reportFalseNegative);
+
+$(document).ready(function() {
+    _domEventHandler(
+        [{
+            type: "bodyInit",
+            target: document.body
+        }]
+    );
+});
+
 observer.observe(document.body, observerInit);
+
+kango.console.log("Observing...");
