19afb4fc3b33f9e65f890eafdf1d4d32c8ef3506
Yavor <yavorpap@abv.bg>
2015-04-25 20:16:19 +0300

    Updated the Blocker class

--- src/common/core/parse/blocker.js
+++ src/common/core/parse/blocker.js
@@ -1,8 +1,9 @@
-function Blocker() {
+function Blocker(markAsAcceptableCallback, markAsOffensiveCallback) {
     this.block(domElement) {
         
     }
     
     this.unblock(domElement) {
+        
     }
 }
