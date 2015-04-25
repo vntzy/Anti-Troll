2c380cfd67e1b9b6206387963935580ec5b118eb
Mario Daskalov <mario@browswave.com>
2015-04-25 19:22:06 +0300

    Crawlers db initial commit

---
+++ crawlers/scripts/create_db.rb
@@ -0,0 +1,27 @@
+require 'sequel'
+
+db_path = "#{File.expand_path(File.dirname(__FILE__))}/../news.db"
+DB = Sequel.sqlite(db_path)
+
+DB.create_table :sites do
+  primary_key :id
+  String  :name
+  String  :url
+end
+
+DB.create_table :news do
+  primary_key :id
+  String  :name
+  String  :url
+  String  :body
+  foreign_key :site_id, :sites
+end
+
+DB.create_table :comments do
+  primary_key :id
+  String   :author
+  String   :body
+  Integer  :upvotes
+  Integer  :downvotes
+  foreign_key :news_id, :news
+end
