1d17c403b29b3b1c67bbe024de9c296a1079beda
Yavor <yavorpap@abv.bg>
2015-04-26 00:45:43 +0300

    Removed trash file

--- src/common/core/jquerytest.js
+++
@@ -1,8 +0,0 @@
-// @require dependencies/jquery-2.1.3.min.js
-
-function myFunction() {
-    alert("jquery works apparently!");
-}
-$(document).ready(myFunction);
-
-// NOTE: This is just a test. This file will be removed.
