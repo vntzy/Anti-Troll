be06fc8d7ee73b077d9fbd8c73ae621c9d73bbcf
Mario Daskalov <mario@browswave.com>
2015-04-26 04:59:45 +0300

    Add classification counter to normal classifier

--- crawlers/lib/manual_classifier/manual_classifier.rb
+++ crawlers/lib/manual_classifier/manual_classifier.rb
@@ -18,6 +18,11 @@ get '/:site/:page' do
     left_outer_join(:comment_classifications, comment_id: :comments__id).
     where(comment_classifications__comment_id: nil).
     select_all(:comments).offset(offset).limit(PAGE_SIZE).all
+  total_classified_comments = DB[:comment_classifications].count
   total_comments = DB[:comments].count
-  haml :index, locals: { comments: comments, total_comments: total_comments }
+  haml :index, locals: {
+    comments: comments,
+    total_comments: total_comments ,
+    total_classified_comments: total_classified_comments
+  }
 end
