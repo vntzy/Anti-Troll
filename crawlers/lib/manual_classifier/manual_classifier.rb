a9853f1794f25ecaf778c140e9b9ad25ea6ead14
Mario Daskalov <mario@browswave.com>
2015-04-26 04:54:22 +0300

    Add pagging for manual classifier

--- crawlers/lib/manual_classifier/manual_classifier.rb
+++ crawlers/lib/manual_classifier/manual_classifier.rb
@@ -7,13 +7,17 @@ post '/classify' do
   redirect back
 end
 
-get '/:site' do
+PAGE_SIZE = 100
+
+get '/:site/:page' do
+  offset = (params[:page].to_i || 0) * PAGE_SIZE
   comments = DB[:comments].
     join(:news, id: :news_id).
     join(:sites, id: :news__site_id).
     where(sites__name: params[:site]).
     left_outer_join(:comment_classifications, comment_id: :comments__id).
     where(comment_classifications__comment_id: nil).
-    select_all(:comments).all
-  haml :index, locals: { comments: comments }
+    select_all(:comments).offset(offset).limit(PAGE_SIZE).all
+  total_comments = DB[:comments].count
+  haml :index, locals: { comments: comments, total_comments: total_comments }
 end
